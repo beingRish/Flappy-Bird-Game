9fd988af6a93afedbab1c1e2b3e00ee0119b08c5
Rishabh Singh <rishabhjuly11@gmail.com>
2023-07-02 23:30:15 +0530

    Developed the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 
+const BIRD_SIZE = 20;
+const GAME_HEIGHT = 500;
+const GAME_WIDTH = 500;
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [birdPosition, setBirdPosition] = useState(GAME_HEIGHT / 2 - BIRD_SIZE / 2);
+  const [score, setScore] = useState(0)
 
   return (
     <div className='App'>
-      <p>Hello World</p>
+      <div style={{
+        position: 'relative',
+        "backgroundColor": "blue",
+        width: `${GAME_HEIGHT}px`,
+        height: `${GAME_WIDTH}px`,
+      }}>
+        <div style={{
+          position: "absolute",
+          backgroundColor: "red",
+          width: `${BIRD_SIZE}px`,
+          height: `${BIRD_SIZE}px`,
+          borderRadius: "50%",
+          top: `${birdPosition}px`,
+        }}></div>
+      </div>
     </div>
   )
 }
